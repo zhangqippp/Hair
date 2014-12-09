d2f85a2af4e938f4287d619c1d7073de6240c0db
zhangqi <zhangqi@t8.io>
2014-12-10 00:38:51 +0800

    first commit

---
+++ Hair/Classes/HairDetail/HairDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  HairDetailViewController.h
+//  Hair
+//
+//  Created by 琦张 on 14/12/8.
+//  Copyright (c) 2014年 zhphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HairDetailViewController : UIViewController
+
+@end
---
+++ Hair/Classes/HairDetail/HairDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  HairDetailViewController.m
+//  Hair
+//
+//  Created by 琦张 on 14/12/8.
+//  Copyright (c) 2014年 zhphy. All rights reserved.
+//
+
+#import "HairDetailViewController.h"
+
+@interface HairDetailViewController ()
+
+@end
+
+@implementation HairDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
