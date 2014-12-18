e5d7b5636c9800841d8dd00d666e8aba1c0ffa38
zhangqippp <archerzq@126.com>
2014-12-18 14:00:31 +0800

    finish face detect function

--- Hair/Classes/HairDetail/HairDetailViewController.m
+++ Hair/Classes/HairDetail/HairDetailViewController.m
@@ -7,10 +7,14 @@
 //
 
 #import "HairDetailViewController.h"
+#import <MobileCoreServices/MobileCoreServices.h>
+#import <ImageIO/ImageIO.h>
 
-@interface HairDetailViewController ()
+@interface HairDetailViewController ()<UIImagePickerControllerDelegate,UINavigationControllerDelegate>
 
 @property (nonatomic,strong) UIButton *cameraBtn;
+@property (nonatomic,strong) UIImageView *photoView;
+@property (nonatomic,strong) UIImage *originImage;
 
 @end
 
@@ -22,6 +26,33 @@
     self.cameraBtn = [UIButton buttonWithType:UIButtonTypeCustom];
     [self.cameraBtn setTitle:@"拍照" forState:UIControlStateNormal];
     [self.cameraBtn setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+    [self.cameraBtn addTarget:self action:@selector(enterCamera) forControlEvents:UIControlEventTouchUpInside];
+    [self.cameraBtn sizeToFit];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.cameraBtn];
+    
+    self.photoView = [[UIImageView alloc] init];
+    self.photoView.frame = CGRectMake(0, 64, self.view.frame.size.width, self.view.frame.size.height-64);
+    self.photoView.backgroundColor = [UIColor grayColor];
+    [self.view addSubview:self.photoView];
+    
+    [self.photoView addObserver:self forKeyPath:@"image" options:NSKeyValueObservingOptionNew context:nil];
+    
+//    self.originImage = [UIImage imageNamed:@"111"];
+    self.photoView.image = [UIImage imageNamed:@"111"];
+    
+    
+    
+//    CIImage *image = self.originImage.CIImage;
+//    CIContext *detectContext = [CIContext contextWithOptions:nil];                    // 1
+//    NSDictionary *opts = @{ CIDetectorAccuracy : CIDetectorAccuracyHigh };      // 2
+//    CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeFace
+//                                              context:detectContext
+//                                              options:opts];                    // 3
+//    opts = @{ CIDetectorImageOrientation : [[image properties] valueForKey:kCGImagePropertyOrientation] }; // 4
+//    NSArray *features = [detector featuresInImage:image options:opts];        // 5
+    
+//    NSLog(@"features:%@",features);
+
     
     // Do any additional setup after loading the view.
 }
@@ -29,6 +60,11 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc
+{
+    [self.photoView removeObserver:self forKeyPath:@"image" context:nil];
 }
 
 /*
@@ -44,7 +80,78 @@
 #pragma mark - method
 - (void)enterCamera
 {
+    NSLog(@"enter camera...");
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    picker.mediaTypes = [NSArray arrayWithObjects:(NSString *)kUTTypeImage, nil];
+    [self presentViewController:picker animated:YES completion:^{
+        
+    }];
+}
+
+#pragma mark - UIImagePickerControllerDelegate
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
     
+    if ([info[UIImagePickerControllerMediaType] isEqualToString:(NSString *)kUTTypeImage]) {
+        UIImage *originImage = info[UIImagePickerControllerOriginalImage];
+        self.originImage = originImage;
+        self.photoView.image = originImage;
+    }
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - NSKeyValueObserving
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    NSLog(@"image change ...");
+//    CIImage *image = self.originImage.CIImage;
+    
+    int exifOrientation;
+    switch (self.originImage.imageOrientation) {
+        case UIImageOrientationUp:
+            exifOrientation = 1;
+            break;
+        case UIImageOrientationDown:
+            exifOrientation = 3;
+            break;
+        case UIImageOrientationLeft:
+            exifOrientation = 8;
+            break;
+        case UIImageOrientationRight:
+            exifOrientation = 6;
+            break;
+        case UIImageOrientationUpMirrored:
+            exifOrientation = 2;
+            break;
+        case UIImageOrientationDownMirrored:
+            exifOrientation = 4;
+            break;
+        case UIImageOrientationLeftMirrored:
+            exifOrientation = 5;
+            break;
+        case UIImageOrientationRightMirrored:
+            exifOrientation = 7;
+            break;
+        default:
+            break;
+    }
+    
+    NSDictionary *detectorOptions = @{ CIDetectorAccuracy : CIDetectorAccuracyHigh }; // TODO: read doc for more tuneups
+    CIDetector *faceDetector = [CIDetector detectorOfType:CIDetectorTypeFace context:nil options:detectorOptions];
+    
+    NSArray *features = [faceDetector featuresInImage:[CIImage imageWithCGImage:self.originImage.CGImage] options:@{CIDetectorImageOrientation:[NSNumber numberWithInt:exifOrientation]}];
+
+    NSLog(@"features:%@",features);
+    for (CIFaceFeature *feature in features) {
+        NSLog(@"face bounds:%f,%f,%f,%f",feature.bounds.origin.x,feature.bounds.origin.y,feature.bounds.size.width,feature.bounds.size.height);
+    }
 }
 
 @end
