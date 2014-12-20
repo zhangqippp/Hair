39b2b177fb3ede46eb7abfda29bdcc783816db4f
yewei <yewei@yeweideMacBook.local>
2014-12-21 00:21:14 +0800

    上下拉刷新

--- Hair/ViewController.m
+++ Hair/ViewController.m
@@ -7,12 +7,18 @@
 //
 
 #import "ViewController.h"
+#import "MJRefresh.h"
 #import "PhotoViewCell.h"
 #import "HairDetailViewController.h"
 
-@interface ViewController ()<UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout>
+@interface ViewController ()<UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout,MJRefreshBaseViewDelegate>
+{
+    MJRefreshHeaderView *_header;
+    MJRefreshFooterView *_footer;
+}
 
 @property (nonatomic, strong) UICollectionView *collectionView;
+@property (nonatomic, strong) NSMutableArray *photoArray;
 
 @end
 
@@ -20,14 +26,91 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    [self.navigationItem setTitle:@"Hair"];
+    [self.navigationItem setTitle:@"Hair"];
     
     [self.view addSubview:self.collectionView];
+    
+    //下拉刷新
+    MJRefreshHeaderView *header = [MJRefreshHeaderView header];
+    header.scrollView = self.collectionView;
+    header.delegate = self;
+
+    [header beginRefreshing];
+    _header = header;
+    
+    //下拉刷新
+    MJRefreshFooterView *footer = [MJRefreshFooterView footer];
+    footer.scrollView = self.collectionView;
+    footer.delegate = self;
+    _footer = footer;
+}
+
+- (void)dealloc
+{
+    NSLog(@"MJCollectionViewController--dealloc---");
+    [_header free];
+    [_footer free];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)doneWithView:(MJRefreshBaseView *)refreshView
+{
+    // 刷新表格
+    [self.collectionView reloadData];
+    
+    // (最好在刷新表格后调用)调用endRefreshing可以结束刷新状态
+    [refreshView endRefreshing];
+}
+
+#pragma mark - 刷新控件的代理方法
+#pragma mark 开始进入刷新状态
+- (void)refreshViewBeginRefreshing:(MJRefreshBaseView *)refreshView
+{
+    NSLog(@"%@----开始进入刷新状态", refreshView.class);
+    
+//    // 1.添加假数据
+//    for (int i = 0; i<5; i++) {
+//        UIColor *color = [UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:1];
+//        
+//        if ([refreshView isKindOfClass:[MJRefreshHeaderView class]]) {
+//            [_fakeColor insertObject:color atIndex:0];
+//        } else {
+//            [_fakeColor addObject:color];
+//        }
+//    }
+    
+    // 2.2秒后刷新表格UI
+    [self performSelector:@selector(doneWithView:) withObject:refreshView afterDelay:2.0];
+}
+
+#pragma mark 刷新完毕
+- (void)refreshViewEndRefreshing:(MJRefreshBaseView *)refreshView
+{
+    NSLog(@"%@----刷新完毕", refreshView.class);
+}
+
+#pragma mark 监听刷新状态的改变
+- (void)refreshView:(MJRefreshBaseView *)refreshView stateChange:(MJRefreshState)state
+{
+    switch (state) {
+        case MJRefreshStateNormal:
+            NSLog(@"%@----切换到：普通状态", refreshView.class);
+            break;
+            
+        case MJRefreshStatePulling:
+            NSLog(@"%@----切换到：松开即可刷新的状态", refreshView.class);
+            break;
+            
+        case MJRefreshStateRefreshing:
+            NSLog(@"%@----切换到：正在刷新状态", refreshView.class);
+            break;
+        default:
+            break;
+    }
 }
 
 #pragma mark -- Getter
@@ -53,7 +136,7 @@
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return 90;
+    return 12;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
