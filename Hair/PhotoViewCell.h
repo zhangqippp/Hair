3e6797fe5231e624f20d57ec7afc8bf0ca9219a2
yewei <yewei@yeweideMacBook.local>
2014-12-20 16:01:42 +0800

    添加collectionview

---
+++ Hair/PhotoViewCell.h
@@ -0,0 +1,15 @@
+//
+//  PhotoViewCell.h
+//  Hair
+//
+//  Created by yewei on 14/12/20.
+//  Copyright (c) 2014年 zhphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoViewCell : UICollectionViewCell
+
+@property (nonatomic, strong) UIImageView *photoImg;
+
+@end
