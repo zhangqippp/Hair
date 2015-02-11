f27aa55fc614474fd7c6b337fd26221bcc4bdb01
zhangqippp <archerzq@126.com>
2015-02-11 22:39:04 +0800

    change score when hair image moving

--- Hair/Classes/Experience/ExperienceVC.m
+++ Hair/Classes/Experience/ExperienceVC.m
@@ -22,6 +22,7 @@
 @property (nonatomic,strong) UIButton *editBtn;
 @property (nonatomic,strong) UIButton *saveBtn;
 @property (nonatomic,strong) UIButton *resetBtn;
+@property (nonatomic,strong) UILabel *scoreLabel;
 @property (nonatomic,strong) UIImageView *photoView;
 @property (nonatomic,strong) HairDisplayView *hairView;
 
@@ -29,6 +30,8 @@
 @property (nonatomic,strong) UIImageView *QRCodeImg;
 
 @property (nonatomic,assign) CGFloat score;
+@property (nonatomic,assign) CGFloat scoreAfterAdjust;
+@property (nonatomic,assign) CGPoint basePoint;
 
 @end
 
@@ -44,6 +47,7 @@
     self.photoView.userInteractionEnabled = YES;
     [self.view addSubview:self.photoView];
     [self.photoView addObserver:self forKeyPath:@"image" options:NSKeyValueObservingOptionNew context:nil];
+    [self.hairView addObserver:self forKeyPath:@"center" options:NSKeyValueObservingOptionNew context:nil];
     
     self.saveBtn = [UIButton buttonWithType:UIButtonTypeSystem];
     [self.saveBtn setTitle:@"保存" forState:UIControlStateNormal];
@@ -63,6 +67,12 @@
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.resetBtn attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1 constant:-20]];
     [self.resetBtn addConstraint:[NSLayoutConstraint constraintWithItem:self.resetBtn attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:0 multiplier:1 constant:60]];
     [self.resetBtn addConstraint:[NSLayoutConstraint constraintWithItem:self.resetBtn attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:0 multiplier:1 constant:40]];
+    
+    [self.view addSubview:self.scoreLabel];
+    self.scoreLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.scoreLabel attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1 constant:0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.scoreLabel attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1 constant:-20]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.scoreLabel attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:0 multiplier:1 constant:40]];
     
     [self.view addSubview:self.QRCodeView];
     self.QRCodeView.hidden = YES;
@@ -115,6 +125,25 @@
     }
 }
 
+- (void)setScore:(CGFloat)score
+{
+    _score = score;
+    self.scoreAfterAdjust = score;
+}
+
+- (void)setScoreAfterAdjust:(CGFloat)scoreAfterAdjust
+{
+    _scoreAfterAdjust = scoreAfterAdjust;
+    if (_scoreAfterAdjust < 0) {
+        _scoreAfterAdjust = 0;
+    }
+//    if (scoreAfterAdjust < 100) {
+        self.scoreLabel.text = [NSString stringWithFormat:@"  您的得分为：%.1f分  ",_scoreAfterAdjust];
+//    }else{
+//        self.scoreLabel.text = [NSString stringWithFormat:@"  您的得分已突破天际：%.1f分  ",_scoreAfterAdjust];
+//    }
+}
+
 #pragma mark - getter
 - (HairDisplayView *)hairView
 {
@@ -155,6 +184,18 @@
         [_QRCodeView addSubview:hideButton];
     }
     return _QRCodeView;
+}
+
+- (UILabel *)scoreLabel
+{
+    if (!_scoreLabel) {
+        _scoreLabel = [[UILabel alloc] init];
+        _scoreLabel.textColor = UIColorFromRGB(0x057aff);
+        _scoreLabel.backgroundColor = [UIColor whiteColor];
+        _scoreLabel.layer.cornerRadius = 5;
+        _scoreLabel.layer.masksToBounds = YES;
+    }
+    return _scoreLabel;
 }
 
 #pragma mark - method
@@ -211,123 +252,131 @@
 #pragma mark - NSKeyValueObserving
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
 {
-    NSLog(@"image change ...");
-    
-    int exifOrientation;
-    switch (self.originImage.imageOrientation) {
-        case UIImageOrientationUp:
-            exifOrientation = 1;
-            break;
-        case UIImageOrientationDown:
-            exifOrientation = 3;
-            break;
-        case UIImageOrientationLeft:
-            exifOrientation = 8;
-            break;
-        case UIImageOrientationRight:
-            exifOrientation = 6;
-            break;
-        case UIImageOrientationUpMirrored:
-            exifOrientation = 2;
-            break;
-        case UIImageOrientationDownMirrored:
-            exifOrientation = 4;
-            break;
-        case UIImageOrientationLeftMirrored:
-            exifOrientation = 5;
-            break;
-        case UIImageOrientationRightMirrored:
-            exifOrientation = 7;
-            break;
-        default:
-            break;
-    }
-    
-    NSDictionary *detectorOptions = @{ CIDetectorAccuracy : CIDetectorAccuracyHigh }; // TODO: read doc for more tuneups
-    CIDetector *faceDetector = [CIDetector detectorOfType:CIDetectorTypeFace context:nil options:detectorOptions];
-    
-    NSArray *features = [faceDetector featuresInImage:[CIImage imageWithCGImage:self.originImage.CGImage] options:@{CIDetectorImageOrientation:[NSNumber numberWithInt:exifOrientation]}];
-    
-    for (UIImageView *img in self.photoView.subviews) {
-        [img removeFromSuperview];
-    }
-    if (features.count<=0) {
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"未识别到头像，请重拍" message:nil delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
-        [alert show];
-    }
-    
-    for (CIFaceFeature *feature in features) {
+    if ([keyPath isEqualToString:@"center"]) {
         
-        UIImageView *faceView = [[UIImageView alloc] init];
-        CGRect rect = feature.bounds;
-        rect.origin.x *= self.photoView.frame.size.width/self.originImage.size.width;
-        rect.origin.y *= self.photoView.frame.size.width/self.originImage.size.width;
-        rect.size.width *= self.photoView.frame.size.width/self.originImage.size.width;
-        rect.size.height *= self.photoView.frame.size.width/self.originImage.size.width;
-        float a = rect.origin.x;
-        rect.origin.x = rect.origin.y;
-        rect.origin.y = a;
-        faceView.frame = rect;
-        
-        faceView.backgroundColor = [UIColor clearColor];
-        [self.photoView addSubview:faceView];
-        CGRect frame = CGRectMake(0, 0, 480*rect.size.width/240, (480*rect.size.width/240)*800/480);
-        self.hairView.frame = frame;
-        self.hairView.center = CGPointMake(faceView.center.x, faceView.center.y+100);
-        [self.photoView addSubview:self.hairView];
-        
-        //打分机制
-        self.score = 70.0f;
-        
-        CGFloat angleScore = 10.0f;
-        if (ABS(feature.faceAngle) < 5.0) {
-            angleScore = 10.0f;
-        }else if (ABS(feature.faceAngle) < 45.0){
-            angleScore -= angleScore*(ABS(feature.faceAngle)-5.0)/40;
+        CGPoint base = self.basePoint;
+        CGPoint current = self.hairView.center;
+        CGFloat distance = sqrt((base.x-current.x)*(base.x-current.x)+(base.y-current.y)*(base.y-current.y));
+
+        if (distance <= 0) {
+            return;
+        }else if (distance <= 80){
+            self.scoreAfterAdjust = self.score + 5.0f*((rand()%10000)/10000.0f);
         }else{
-            angleScore = 0;
-        }
-        self.score += angleScore;
-        
-        CGFloat locationXScore = 10.0f;
-        if (rect.origin.x<0 || rect.origin.x+rect.size.width>self.photoView.frame.size.width) {
-            locationXScore = 0;
-        }else{
-            locationXScore -= locationXScore*(ABS(((rect.origin.x + rect.size.width)/2) - self.photoView.frame.size.width/2)/(self.photoView.frame.size.width/2 - rect.size.width/2));
-        }
-        self.score += locationXScore;
-        
-        CGFloat locationYScore = 10.0f;
-        if (rect.origin.y<0 || rect.origin.y+rect.size.height>self.photoView.frame.size.height) {
-            locationYScore = 0;
-        }
-        self.score += locationYScore;
-        
-        if (feature.hasSmile) {
-            self.score += 5;
+            self.scoreAfterAdjust = self.score - (distance - 80.0f)*(distance - 80.0f)/200;
         }
         
-        if (!feature.hasLeftEyePosition) {
-            self.score -= 3;
+    }else if ([keyPath isEqualToString:@"image"]){
+        int exifOrientation;
+        switch (self.originImage.imageOrientation) {
+            case UIImageOrientationUp:
+                exifOrientation = 1;
+                break;
+            case UIImageOrientationDown:
+                exifOrientation = 3;
+                break;
+            case UIImageOrientationLeft:
+                exifOrientation = 8;
+                break;
+            case UIImageOrientationRight:
+                exifOrientation = 6;
+                break;
+            case UIImageOrientationUpMirrored:
+                exifOrientation = 2;
+                break;
+            case UIImageOrientationDownMirrored:
+                exifOrientation = 4;
+                break;
+            case UIImageOrientationLeftMirrored:
+                exifOrientation = 5;
+                break;
+            case UIImageOrientationRightMirrored:
+                exifOrientation = 7;
+                break;
+            default:
+                break;
         }
         
-        if (!feature.hasRightEyePosition) {
-            self.score -= 3;
+        NSDictionary *detectorOptions = @{ CIDetectorAccuracy : CIDetectorAccuracyHigh }; // TODO: read doc for more tuneups
+        CIDetector *faceDetector = [CIDetector detectorOfType:CIDetectorTypeFace context:nil options:detectorOptions];
+        
+        NSArray *features = [faceDetector featuresInImage:[CIImage imageWithCGImage:self.originImage.CGImage] options:@{CIDetectorImageOrientation:[NSNumber numberWithInt:exifOrientation]}];
+        
+        for (UIImageView *img in self.photoView.subviews) {
+            [img removeFromSuperview];
+        }
+        if (features.count<=0) {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"未识别到头像，请重拍" message:nil delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
+            [alert show];
         }
         
-        if (!feature.hasMouthPosition) {
-            self.score -= 3;
+        for (CIFaceFeature *feature in features) {
+            
+            UIImageView *faceView = [[UIImageView alloc] init];
+            CGRect rect = feature.bounds;
+            rect.origin.x *= self.photoView.frame.size.width/self.originImage.size.width;
+            rect.origin.y *= self.photoView.frame.size.width/self.originImage.size.width;
+            rect.size.width *= self.photoView.frame.size.width/self.originImage.size.width;
+            rect.size.height *= self.photoView.frame.size.width/self.originImage.size.width;
+            float a = rect.origin.x;
+            rect.origin.x = rect.origin.y;
+            rect.origin.y = a;
+            faceView.frame = rect;
+            
+            faceView.backgroundColor = [UIColor clearColor];
+            [self.photoView addSubview:faceView];
+            CGRect frame = CGRectMake(0, 0, 480*rect.size.width/240, (480*rect.size.width/240)*800/480);
+            self.hairView.frame = frame;
+            self.basePoint = CGPointMake(faceView.center.x, faceView.center.y+100);
+            self.hairView.center = CGPointMake(faceView.center.x, faceView.center.y+100);
+            [self.photoView addSubview:self.hairView];
+            
+            //打分机制
+            self.score = 70.0f;
+            
+            CGFloat angleScore = 10.0f;
+            if (ABS(feature.faceAngle) < 5.0) {
+                angleScore = 10.0f;
+            }else if (ABS(feature.faceAngle) < 45.0){
+                angleScore -= angleScore*(ABS(feature.faceAngle)-5.0)/40;
+            }else{
+                angleScore = 0;
+            }
+            self.score += angleScore;
+            
+            CGFloat locationXScore = 10.0f;
+            if (rect.origin.x<0 || rect.origin.x+rect.size.width>self.photoView.frame.size.width) {
+                locationXScore = 0;
+            }else{
+                locationXScore -= locationXScore*(ABS(((rect.origin.x + rect.size.width)/2) - self.photoView.frame.size.width/2)/(self.photoView.frame.size.width/2 - rect.size.width/2));
+            }
+            self.score += locationXScore;
+            
+            CGFloat locationYScore = 10.0f;
+            if (rect.origin.y<0 || rect.origin.y+rect.size.height>self.photoView.frame.size.height) {
+                locationYScore = 0;
+            }
+            self.score += locationYScore;
+            
+            if (feature.hasSmile) {
+                self.score += 5;
+            }
+            
+            if (!feature.hasLeftEyePosition) {
+                self.score -= 3;
+            }
+            
+            if (!feature.hasRightEyePosition) {
+                self.score -= 3;
+            }
+            
+            if (!feature.hasMouthPosition) {
+                self.score -= 3;
+            }
         }
-        
-        NSString *scoreInfo = @"";
-        if (self.score < 100) {
-            scoreInfo = [NSString stringWithFormat:@"您的得分为：%.0f分",self.score];
-        }else{
-            scoreInfo = [NSString stringWithFormat:@"您的得分已突破天际：%.0f分",self.score];
-        }
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil message:scoreInfo delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
-        [alert show];
+
     }
+    
 }
 
 #pragma mark - UIAlertViewDelegate
